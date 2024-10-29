b8ecb12a74743ff50293aa9f1e470198c3d748e8
dariatacu <tacu.daria.r5q@student.ucv.ro>
2024-10-29 17:55:19 +0200

    curs 1 vectori

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include<vector>
 
 using namespace std;
 
@@ -10,14 +11,17 @@ int main()
 
     int no_problems, no_doctors;
     string name, speciality;
-    
+    vector < pair<string, string>>problems;
+    vector < pair<string, string>>doctors;
+
     inFile >> no_problems;
 
     for (int i = 0; i < no_problems; i++)
     {
         inFile >> name;
         inFile >> speciality;
-        cout << name << ' ' << speciality << '\n';
+        //cout << name << ' ' << speciality << '\n';
+        problems.push_back(make_pair(name, speciality));
     }
 
     inFile >> no_doctors;
@@ -26,7 +30,25 @@ int main()
     {
         inFile >> name;
         inFile >> speciality;
-        cout << name << ' ' << speciality << '\n';
+        //cout << name << ' ' << speciality << '\n';
+        doctors.push_back(make_pair(name, speciality));
+    }
+    for (auto problem : problems)
+    {
+        bool ok = false;
+        for (auto doctor : doctors)
+        {
+            if (problem.second == doctor.second)
+            {
+                cout << problem.first << " " << "Acceptat" << "\n";
+                ok = true;
+            }
+
+        }
+        if (!ok)
+        {
+            cout << problem.first << " " << "Respins" << "\n";
+        }
     }
 
     return 0;
